d33b6f1053f1a43713ce9fc9d6338a85b7e42bba
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-01 09:28:17 +0300

    Добавлена модель входа пользователя в аккаунт.

---
+++ Models/LoginUserModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP_MVC_Project.Models
+{
+    public class LoginUserModel
+    {
+        [Required(ErrorMessage = "Не указан Login")]
+        [DataType(DataType.Text)]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Не указан Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
