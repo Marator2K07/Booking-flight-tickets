00355a12d4c246690977ba3e5abb8fa04e380659
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-01 08:43:22 +0300

    Класс пользователя User для авторизации и хранения билетов.

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace ASP_MVC_Project.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string DocumentNumber { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public virtual int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+        public virtual ICollection<Airticket> Airtickets { get; set; }
+    }
+}
