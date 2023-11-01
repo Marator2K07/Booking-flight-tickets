093a07df57c92a5ac298d4d7eabf1dd895b0eaaf
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-01 08:50:41 +0300

    Добавлена связь с классом Schedule.

--- Models/Airline.cs
+++ Models/Airline.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string License { get; set; }
+        public virtual int ScheduleId { get; set; }
+        public virtual Schedule Schedule { get; set; }
     }
 }
