e91ddb14f5ff90d571d2ee565eecb6fb52e1cc1c
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-01 08:46:03 +0300

    Класс расписания Schedule. Для хранения билетов и авиакомпаний.

---
+++ Models/Schedule.cs
@@ -0,0 +1,10 @@
+﻿namespace ASP_MVC_Project.Models
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public virtual ICollection<Airticket> Airtickets { get; set; }
+        public virtual ICollection<Airline> Airlines { get; set; }
+    }
+}
