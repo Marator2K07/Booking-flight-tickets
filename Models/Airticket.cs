c960e52b75939aee10cd30d9a6f57d18d920fafe
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-01 08:49:54 +0300

    Создан класс Aviaticket. Содержится в расписании и может быть связан с юзером-покупателем.

---
+++ Models/Airticket.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP_MVC_Project.Models
+{
+    public class Airticket
+    {
+        public int Id { get; set; }
+        public virtual int? UserId { get; set; }
+        public virtual User? User { get; set; }
+        public virtual int ScheduleId { get; set; }
+        public virtual Schedule Schedule { get; set; }
+    }
+}
