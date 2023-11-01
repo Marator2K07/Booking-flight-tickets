f0bfaa165d167135711a9d482f7f8fd8a752f34f
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-01 08:22:07 +0300

    Добавлен класс Airline в модель для хранения данных об авиакомпаниях.

---
+++ Models/Airline.cs
@@ -0,0 +1,9 @@
+﻿namespace ASP_MVC_Project.Models
+{
+    public class Airline
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string License { get; set; }
+    }
+}
