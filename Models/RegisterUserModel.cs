8725c47e2135281f8593db53a9be97360f65b9dc
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-01 09:27:49 +0300

    Добавлена модель регистрации пользователя.

---
+++ Models/RegisterUserModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASP_MVC_Project.Models
+{
+    public class RegisterUserModel
+    {
+        [Required(ErrorMessage = "Не указано имя пользователя")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Не указана фамилия пользователя")]
+        [DataType(DataType.Text)]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Не указан номер документа пользователя")]
+        [DataType(DataType.Text)]
+        public string DocumentNumber { get; set; }
+
+        [Required(ErrorMessage = "Не указан логин пользователя")]
+        [DataType(DataType.Text)]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Забыли указать пароль для пользователя")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Пароли не совпадают, попробуйте еще")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
