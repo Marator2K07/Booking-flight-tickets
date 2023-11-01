c8b9d7b42928da3c8113754a3c04d9bc713bfd6a
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-01 09:10:55 +0300

    Добавлен класс контекста базы данных.

---
+++ Models/FlightsDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Azure;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_MVC_Project.Models
+{
+    public class FlightsDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Airline> Airlines { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Airticket> Airtickets { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+        public FlightsDbContext(DbContextOptions options) :
+            base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Role>().HasData(
+                new Role { Id = 1, Name = "Standart" },
+                new Role { Id = 2, Name = "Administrator" }
+            );
+        }
+    }
+}
