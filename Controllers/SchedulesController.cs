edc9aef78c46b579ba5a0fd76e3db27f81e89336
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-03 10:11:05 +0300

    Добавлен котроллер расписания перелетов.

---
+++ Controllers/SchedulesController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ASP_MVC_Project.Models;
+
+namespace ASP_MVC_Project.Controllers
+{
+    public class SchedulesController : Controller
+    {
+        private readonly FlightsDbContext _context;
+
+        public SchedulesController(FlightsDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var flightsDbContext = _context.Schedules.Include(s => s.Airline);
+            return View(await flightsDbContext.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Schedules == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedules
+                .Include(s => s.Airline)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            return View(schedule);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewData["AirlineId"] = new SelectList(_context.Airlines, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Date,Departure,Destination,AirlineId")] Schedule schedule)
+        {
+            _context.Add(schedule);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Schedules == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedules.FindAsync(id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+            ViewData["AirlineId"] = new SelectList(_context.Airlines, "Id", "Name", schedule.AirlineId);
+            return View(schedule);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Date,Departure,Destination,AirlineId")] Schedule schedule)
+        {
+            if (id != schedule.Id)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Update(schedule);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ScheduleExists(schedule.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            ViewData["AirlineId"] = new SelectList(_context.Airlines, "Id", "Name", schedule.AirlineId);
+            return View(schedule);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Schedules == null)
+            {
+                return NotFound();
+            }
+
+            var schedule = await _context.Schedules
+                .Include(s => s.Airline)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            return View(schedule);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Schedules == null)
+            {
+                return Problem("Entity set 'FlightsDbContext.Schedules'  is null.");
+            }
+            var schedule = await _context.Schedules.FindAsync(id);
+            if (schedule != null)
+            {
+                _context.Schedules.Remove(schedule);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ScheduleExists(int id)
+        {
+            return (_context.Schedules?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
