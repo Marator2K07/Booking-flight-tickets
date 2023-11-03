31a1512c4476df95468e1c9ad4e72779e6cdaf1f
MaRaToR2K07 <dmitriyshaaa@gmail.com>
2023-11-03 11:39:35 +0300

    Начальная реализация контроллера покупки авиабилетов. Пока есть только страница с общей информацией о перелетах.

---
+++ Controllers/AirticketsController.cs
@@ -0,0 +1,24 @@
+﻿using ASP_MVC_Project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_MVC_Project.Controllers
+{
+    public class AirticketsController : Controller
+    {
+        private readonly FlightsDbContext _context;
+
+        public AirticketsController(FlightsDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Booking()
+        {
+            var flightsDbContext = _context.Schedules.Include(s => s.Airline);
+            return View(await flightsDbContext.ToListAsync());
+        }
+    }
+}
